3c9fa47443560cdb57b373a793bdf61477939049
Redwan <redwan.mahmud07@gmail.com>
2021-03-16 22:32:46 -0700

    did some work on the header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 import Search from "./components/Search";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import List from "./components/List/List";
+
+import stays from "./source/stays.json";
 const App = () => {
+  const [homes, setHomes] = useState(stays);
+
+  //declare states
+  // need to make a call back
   return (
     <div>
       <Header />
       <Search />
-      <List />
+      <List homes={homes} />
       <Footer />
     </div>
   );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const List = () => {
-  return <div>Display List Here</div>;
+const List = ({ homes }) => {
+  console.log(homes);
+  const listItems = homes.map((home) => (
+    <li>
+      {home.city} {home.rating} {home.superHost ? "SuperHost" : "Not superhost"}
+    </li>
+  ));
+  return (
+    <div>
+      <h1> Display List Here </h1>
+      <ul>{listItems}</ul>
+    </div>
+  );
 };
 
 export default List;
